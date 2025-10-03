f19f5e3658f4a9e428ce5881097e37901340ac22
Adrian González Pardo <gozapaadr@gmail.com>
2025-10-03 00:37:21 -0600

    fix: Add soft delete base model for processing information and use in backend app

---
+++ wardrive/apps/core/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.db.models.query import QuerySet
+from django.utils.translation import pgettext_lazy
+from django.utils.timezone import now
+
+from apps.wardriving import SourceDevice
+
+
+class SoftQuerySet(QuerySet):
+    def delete(self):
+        # Actualiza el estado de eliminacion logica
+        return super(SoftQuerySet, self).update(deleted_at=now())
+
+    def hard_delete(self):
+        # Elimina totalmente el objeto
+        return super(SoftQuerySet, self).delete()
+
+    def alive(self):
+        # Filtra aquellos objetos activos
+        return self.filter(deleted_at__isnull=True)
+
+    def dead(self):
+        # Filtra aquellos objetos eliminados logicamente
+        return self.exclude(deleted_at__isnull=True)
+
+
+class SoftDeleteManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop("alive_only", True)
+        super(SoftDeleteManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftQuerySet(self.model).filter(deleted_at__isnull=self.alive_only)
+        return SoftQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+
+class WardriveBaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=pgettext_lazy("Wardrive Base Model field", "created at"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=pgettext_lazy("Wardrive Base Model field", "updated at"),
+    )
+    deleted_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        verbose_name=pgettext_lazy("Wardrive Base Model field", "deleted at"),
+    )
+    first_seen = models.DateTimeField(verbose_name="First Seen", default=now)
+    uploaded_by = models.TextField(verbose_name="Uploaded by", default="")
+    device_source = models.CharField(
+        max_length=50,
+        verbose_name="Source",
+        choices=SourceDevice.CHOICES,
+        default=SourceDevice.UNKNOWN,
+    )
+
+    objects = SoftDeleteManager()
+    all_objects = SoftDeleteManager(alive_only=False)
+
+    class Meta:
+        abstract = True
+
+    def can_delete(self):
+        return True
+
+    def delete(self):
+        self.can_delete()
+        # Permite hacer un delete logico en la BD
+        self.deleted_at = now()
+        self.save()
+
+    def recover(self):
+        # Permite recuperar un objeto
+        self.deleted_at = None
+        self.save()
+
+    def hard_delete(self):
+        # Hace una eliminacion directa
+        super().delete()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super().save(*args, **kwargs)
--- wardrive/apps/wardriving/models.py
+++ wardrive/apps/wardriving/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.utils.timezone import now
 
 from . import SourceDevice
+from apps.core.models import WardriveBaseModel
 
 
-class Wardriving(models.Model):
+class Wardriving(WardriveBaseModel):
     mac = models.CharField(
-        max_length=17, verbose_name="MAC Address"
+        max_length=17, verbose_name="MAC Address", default="AA:BB:CC:DD:EE:FF"
     )  # Format: XX:XX:XX:XX:XX:XX
     ssid = models.CharField(max_length=255, verbose_name="SSID", default="")
     auth_mode = models.CharField(
         max_length=50, verbose_name="Authentication Mode", default=""
     )
-    first_seen = models.DateTimeField(verbose_name="First Seen", default=now)
     channel = models.IntegerField(verbose_name="Channel")
     rssi = models.IntegerField(verbose_name="RSSI (Signal Strength)")
     current_latitude = models.DecimalField(
@@ -31,13 +31,6 @@ class Wardriving(models.Model):
         max_digits=6, decimal_places=2, verbose_name="Accuracy (Meters)", default=0
     )
     type = models.CharField(max_length=50, verbose_name="Type", default="WIFI")
-    device_source = models.CharField(
-        max_length=50,
-        verbose_name="Source",
-        choices=SourceDevice.CHOICES,
-        default=SourceDevice.UNKNOWN,
-    )
-    uploaded_by = models.TextField(verbose_name="Uploaded by", default="")
 
     class Meta:
         db_table = "wardriving"
@@ -45,13 +38,14 @@ class Wardriving(models.Model):
         verbose_name_plural = "Wardriving Data"
 
     def __str__(self):
-        return (f"{(self.ssid or self.type) or "Unknown Device"} ({self.mac})")
+        device = self.ssid or self.type or "Unknown Device"
+        return f"{device} ({self.mac})"
 
     def is_default_data(self):
         return self.current_latitude == 0 and self.current_longitude == 0
 
 
-class LTEWardriving(models.Model):
+class LTEWardriving(WardriveBaseModel):
     # From csv device content file
     mcc = models.IntegerField(verbose_name="MCC (Mobile Country Code)")
     mnc = models.IntegerField(verbose_name="MNC (Mobile Network Code)")
@@ -72,15 +66,6 @@ class LTEWardriving(models.Model):
         max_digits=9, decimal_places=6, verbose_name="Longitude", default=0
     )
     tech = models.TextField(verbose_name="Technology", default="LTE")
-    # Custom config for conquest and identify origin of source and when its founded and by who
-    first_seen = models.DateTimeField(verbose_name="First Seen", default=now)
-    uploaded_by = models.TextField(verbose_name="Uploaded by", default="")
-    device_source = models.CharField(
-        max_length=50,
-        verbose_name="Source",
-        choices=SourceDevice.CHOICES,
-        default=SourceDevice.UNKNOWN,
-    )
 
     class Meta:
         db_table = "lte_wardriving"
